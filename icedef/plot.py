94264f56e72448cfa22a9d98973a7871a9bdee0f
evankielley <evankielley@gmail.com>
2019-02-20 15:58:40 -0330

    changed font sizes

--- icedef/plot.py
+++ icedef/plot.py
@@ -10,15 +10,15 @@ from pandas import to_datetime
 
 SMALL_SIZE = 10
 MEDIUM_SIZE = 12
-BIGGER_SIZE = 14
+LARGE_SIZE = 14
 
 plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=BIGGER_SIZE)     # fontsize of the x and y labels
+plt.rc('axes', titlesize=LARGE_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=LARGE_SIZE)     # fontsize of the x and y labels
 plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=MEDIUM_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)   # fontsize of the figure title
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=LARGE_SIZE)   # fontsize of the figure title
 
 
 def get_map(**kwargs):
